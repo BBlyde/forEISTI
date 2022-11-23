13fab397306edb344711570813835f059cb22789
Mateo Mongour <mateo.mongour@eisti.fr>
2022-11-23 12:56:35 +0100

    Added mysql config

--- src/main/java/foreisti/ProjetJeeApplication.java
+++ src/main/java/foreisti/ProjetJeeApplication.java
@@ -1,4 +1,4 @@
-package fr.couilles.projet_jee;
+package foreisti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+	<body>
+		nique
+	</body>
+</html>
